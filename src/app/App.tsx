aa863aae88b83ae4ba9dbc1ed13836bbc8d341be
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-08-28 22:56:30 +0300

    upd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -120,7 +120,7 @@ function App() {
 
                 <Container fixed>
                     <Routes>
-                        <Route path={'/'} element={<TodolistsList/>}/>
+                        {/*<Route path={'/'} element={<TodolistsList/>}/>*/}
                         <Route path={'/todolist-ts-react/'} element={<TodolistsList/>}/>
                         <Route path={'/login'} element={<Login/>}/>
                     </Routes>
